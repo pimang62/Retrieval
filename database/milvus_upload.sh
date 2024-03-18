a88f50ec993523dc57567e5e8f9246affc1a8292
pimang62 <121668884+pimang62@users.noreply.github.com>
2024-03-18 09:17:16 +0900

    delete

--- database/milvus_upload.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# nohup python baemin_upload.py --file_path /home/pimang62/projects/qachat/a245_chatgpt_knowledge_chatbot/api/json/openai_emb.json --collection_name baemin > logs/baemin.log 2>&1 &
-
-# nohup python baemin_upload.py --file_path /home/pimang62/projects/qachat/a245_chatgpt_knowledge_chatbot/api/json/openai_expert_emb.json --collection_name baemin_expert > logs/baemin_expert.log 2>&1 &
-
-nohup python baemin_upload.py --file_path /home/pimang62/projects/qachat/a245_chatgpt_knowledge_chatbot/api/json/openai_expert_emb.json --collection_name baemin_expert_qonly > logs/baemin_expert_qonly.log 2>&1 &
